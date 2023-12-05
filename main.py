a21021e136be8f9d575816b7f617463f80502c4b
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2023-12-04 16:37:10 -0800

    playing with functions

--- main.py
+++ main.py
@@ -10,38 +10,41 @@ class Insult(BaseModel):
     insult: str
 
 insults = {
-    "s1": [
-        "s1 insult 1",
-        "s1 insult 2",
-        "s1 insult 3",
-    ],
-    "s2": [
-        "s2 insult 1",
-        "s2 insult 2",
-        "s2 insult 3",
-    ],
+    "insults": [
+        "insult one",
+        "insult two",
+        "insult three",
+    ]
 }
 
 @silicon_valley_insults.get("/")
 def read_root():
     return FileResponse("static/index.html")
 
-@silicon_valley_insults.get('/insult', response_model=Insult)
+@silicon_valley_insults.get("/api/insults", response_model=Insult)
 async def get_random_insult():
-    all_insults = []
-    for season in insults:
-        insult_list = insults[season]
-        for insult in insult_list:
-            all_insults.append(insult)
+    all_insults = insults.get("insults")
     if not all_insults:
         raise HTTPException(status_code=404, detail="No insults available")
     random_insult = random.choice(all_insults)
     return {"insult": random_insult}
 
-@silicon_valley_insults.get('/insult/{season}', response_model=Insult)
-async def get_season_insult(season: str):
-    season_insults = insults.get(season, [])
-    if not season_insults:
-         raise HTTPException(status_code=404, detail=f"No {season} insults available")
-    random_insult = random.choice(season_insults)
-    return {"insult": random_insult}
+# @silicon_valley_insults.get("/api/insults", response_model=Insult)
+# async def get_random_insult():
+#     all_insults = []
+#     for season in insults:
+#         insult_list = insults[season]
+#         for insult in insult_list:
+#             all_insults.append(insult)
+#     if not all_insults:
+#         raise HTTPException(status_code=404, detail="No insults available")
+#     random_insult = random.choice(all_insults)
+#     return {"insult": random_insult}
+
+# @silicon_valley_insults.get('/insult/{season}', response_model=Insult)
+# async def get_season_insult(season: str):
+#     season_insults = insults.get(season, [])
+#     if not season_insults:
+#          raise HTTPException(status_code=404, detail=f"No {season} insults available")
+#     random_insult = random.choice(season_insults)
+#     return {"insult": random_insult}
