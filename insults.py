c48e54ec208458316e1f1a2db28aa38e0ac50454
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2023-12-07 18:07:56 -0800

    added more endpoints

--- insults.py
+++ insults.py
@@ -7,22 +7,36 @@ insults = {
         {
             "season": 1,
             "episode": 1,
-            "character": "Erlich Bachman",
+            "character": "erlich",
             "insult": "Okay, there's 40 billion dollars of net worth walking around this party. And you guys are standing around drinking shrimp and talking about what cum tastes like.",
         },
 
         {
             "season": 1,
             "episode": 1,
-            "character": "Dinesh Chugtai",
+            "character": "dinesh",
             "insult": "Boy, is she on the spectrum. She can't even make eye contact with the camera.",
         },
 
         {
             "season": 1,
             "episode": 1,
-            "character": "Erlich Bachman",
+            "character": "erlich",
             "insult": "You just disappeared up your own asshold. Did you know that?",
         },
     ]
 }
+
+
+
+# s1 e2
+
+# We'll call you when we want pleated khakis.
+
+# While you were busy minoring in gender studies and singing a capella at Sarah Lawrence, I was gaining root access to NSA servers.
+
+# I know Gilfoyle probably came in here and puked out a bunch of tech specs, three-fourths of which are total horse-sh*t.
+
+# I hate Richard Hendricks, that little Pied Piper prick.
+
+# https://transcripts.foreverdreaming.org/viewtopic.php?t=26837&sid=eda5d3c7e3cdec64d5db21059c32301a
--- main.py
+++ main.py
@@ -30,22 +30,85 @@ async def get_random_insult():
         insult=random_insult["insult"]
     )
 
-# @silicon_valley_insults.get("/api/insults", response_model=Insult)
-# async def get_random_insult():
-#     all_insults = []
-#     for season in insults:
-#         insult_list = insults[season]
-#         for insult in insult_list:
-#             all_insults.append(insult)
-#     if not all_insults:
-#         raise HTTPException(status_code=404, detail="No insults available")
-#     random_insult = random.choice(all_insults)
-#     return {"insult": random_insult}
+@silicon_valley_insults.get("/api/insults/season/{season}", response_model=DetailedInsult)
+async def get_insult_by_season(season: int):
+    season_insults = [insult for insult in insults["insults"] if insult["season"] == season]
+    if not season_insults:
+        raise HTTPException(status_code=404, detail=f"No insults available for season {season}")
+    random_insult = random.choice(season_insults)
+    return DetailedInsult(
+        season=random_insult["season"],
+        episode=random_insult["episode"],
+        character=random_insult["character"],
+        insult=random_insult["insult"]
+    )
 
-# @silicon_valley_insults.get('/insult/{season}', response_model=Insult)
-# async def get_season_insult(season: str):
-#     season_insults = insults.get(season, [])
-#     if not season_insults:
-#          raise HTTPException(status_code=404, detail=f"No {season} insults available")
-#     random_insult = random.choice(season_insults)
-#     return {"insult": random_insult}
+@silicon_valley_insults.get("/api/insults/season/{season}/episode/{episode}", response_model=DetailedInsult)
+async def get_insult_by_season_and_episode(season: int, episode: int):
+    episode_insults = [
+        insult for insult in insults["insults"]
+        if insult["season"] == season and insult["episode"] == episode
+    ]
+    if not episode_insults:
+        raise HTTPException(
+            status_code=404,
+            detail=f"No insults available for season {season}, episode {episode}"
+        )
+    random_insult = random.choice(episode_insults)
+    return DetailedInsult(
+        season=random_insult["season"],
+        episode=random_insult["episode"],
+        character=random_insult["character"],
+        insult=random_insult["insult"]
+    )
+
+@silicon_valley_insults.get("/api/insults/character/{character}", response_model=DetailedInsult)
+async def get_insult_by_character(character: str):
+    character_insults = [insult for insult in insults["insults"] if insult["character"] == character]
+    if not character_insults:
+        raise HTTPException(status_code=404, detail=f"No insults available for character {character}")
+    random_insult = random.choice(character_insults)
+    return DetailedInsult(
+        season=random_insult["season"],
+        episode=random_insult["episode"],
+        character=random_insult["character"],
+        insult=random_insult["insult"]
+    )
+
+@silicon_valley_insults.get("/api/insults/season/{season}/character/{character}", response_model=DetailedInsult)
+async def get_insult_by_season_and_character(season: int, character: str):
+    season_character_insults = [
+        insult for insult in insults["insults"]
+        if insult["season"] == season and insult["character"] == character
+    ]
+    if not season_character_insults:
+        raise HTTPException(
+            status_code=404,
+            detail=f"No insults available for season {season} and character {character}"
+        )
+    random_insult = random.choice(season_character_insults)
+    return DetailedInsult(
+        season=random_insult["season"],
+        episode=random_insult["episode"],
+        character=random_insult["character"],
+        insult=random_insult["insult"]
+    )
+
+@silicon_valley_insults.get("/api/insults/season/{season}/episode/{episode}/character/{character}", response_model=DetailedInsult)
+async def get_insult_by_season_episode_and_character(season: int, episode: int, character: str):
+    episode_character_insults = [
+        insult for insult in insults["insults"]
+        if insult["season"] == season and insult["episode"] == episode and insult["character"] == character
+    ]
+    if not episode_character_insults:
+        raise HTTPException(
+            status_code=404,
+            detail=f"No insults available for season {season}, episode {episode}, and character {character}"
+        )
+    random_insult = random.choice(episode_character_insults)
+    return DetailedInsult(
+        season=random_insult["season"],
+        episode=random_insult["episode"],
+        character=random_insult["character"],
+        insult=random_insult["insult"]
+    )
