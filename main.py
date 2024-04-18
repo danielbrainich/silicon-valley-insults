a7b4364e3cdf6dacf79decf88740bd04be7a0949
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2024-04-18 10:21:09 -0700

    added rate limiting

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ from pydantic import BaseModel
 import random
 from insults import insults
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Request
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
 
+limiter = Limiter(key_func=get_remote_address)
 
-silicon_valley_insults = FastAPI()
-silicon_valley_insults.mount("/static", StaticFiles(directory="static"), name="static")
+app = FastAPI()
 
-silicon_valley_insults.add_middleware(
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=False,
@@ -26,12 +35,13 @@ class DetailedInsult(BaseModel):
     character: str
     insult: str
 
-@silicon_valley_insults.get("/")
+@app.get("/")
 def read_root():
     return FileResponse("static/index.html")
 
-@silicon_valley_insults.get("/api/insults", response_model=DetailedInsult)
-async def get_random_insult():
+@app.get("/api/insults", response_model=DetailedInsult)
+@limiter.limit("100/hour")
+async def get_random_insult(request: Request):
     all_insults = insults["insults"]
     if not all_insults:
         raise HTTPException(status_code=404, detail="No insults available")
@@ -43,8 +53,9 @@ async def get_random_insult():
         insult=random_insult["insult"]
     )
 
-@silicon_valley_insults.get("/api/insults/season/{season}", response_model=DetailedInsult)
-async def get_insult_by_season(season: int):
+@app.get("/api/insults/season/{season}", response_model=DetailedInsult)
+@limiter.limit("100/hour")
+async def get_insult_by_season(request: Request, season: int):
     season_insults = [insult for insult in insults["insults"] if insult["season"] == season]
     if not season_insults:
         raise HTTPException(status_code=404, detail=f"No insults available for season {season}")
@@ -56,8 +67,9 @@ async def get_insult_by_season(season: int):
         insult=random_insult["insult"]
     )
 
-@silicon_valley_insults.get("/api/insults/season/{season}/episode/{episode}", response_model=DetailedInsult)
-async def get_insult_by_season_and_episode(season: int, episode: int):
+@app.get("/api/insults/season/{season}/episode/{episode}", response_model=DetailedInsult)
+@limiter.limit("100/hour")
+async def get_insult_by_season_and_episode(request: Request, season: int, episode: int):
     episode_insults = [
         insult for insult in insults["insults"]
         if insult["season"] == season and insult["episode"] == episode
@@ -75,8 +87,9 @@ async def get_insult_by_season_and_episode(season: int, episode: int):
         insult=random_insult["insult"]
     )
 
-@silicon_valley_insults.get("/api/insults/character/{character}", response_model=DetailedInsult)
-async def get_insult_by_character(character: str):
+@app.get("/api/insults/character/{character}", response_model=DetailedInsult)
+@limiter.limit("100/hour")
+async def get_insult_by_character(request: Request, character: str):
     character_insults = [insult for insult in insults["insults"] if (insult["character"]).lower() == character]
     if not character_insults:
         raise HTTPException(status_code=404, detail=f"No insults available for character {(character).title()}")
@@ -88,8 +101,9 @@ async def get_insult_by_character(character: str):
         insult=random_insult["insult"]
     )
 
-@silicon_valley_insults.get("/api/insults/season/{season}/character/{character}", response_model=DetailedInsult)
-async def get_insult_by_season_and_character(season: int, character: str):
+@app.get("/api/insults/season/{season}/character/{character}", response_model=DetailedInsult)
+@limiter.limit("100/hour")
+async def get_insult_by_season_and_character(request: Request, season: int, character: str):
     season_character_insults = [
         insult for insult in insults["insults"]
         if insult["season"] == season and (insult["character"]).lower() == character
@@ -107,8 +121,9 @@ async def get_insult_by_season_and_character(season: int, character: str):
         insult=random_insult["insult"]
     )
 
-@silicon_valley_insults.get("/api/insults/season/{season}/episode/{episode}/character/{character}", response_model=DetailedInsult)
-async def get_insult_by_season_episode_and_character(season: int, episode: int, character: str):
+@app.get("/api/insults/season/{season}/episode/{episode}/character/{character}", response_model=DetailedInsult)
+@limiter.limit("100/hour")
+async def get_insult_by_season_episode_and_character(request: Request, season: int, episode: int, character: str):
     episode_character_insults = [
         insult for insult in insults["insults"]
         if insult["season"] == season and insult["episode"] == episode and (insult["character"]).lower() == character
